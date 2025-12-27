d1419639c1b05633abaf0967796681ae28602207
Kévin ʘ‿ʘ <kevin@solcial.io>
2025-12-27 06:10:47 +0100

    wip
    # Conflicts:
    #	src/App.vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import { router } from './router'
 
-createApp(App).mount('#app')
+import App from './app.vue'
+
+createApp(App)
+	.use(router)
+	.mount('#app')
