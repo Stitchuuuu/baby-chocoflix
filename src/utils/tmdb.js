3c9dda9f0c4f5fceaf110a59a713ac9319b21961
Kévin ʘ‿ʘ <kevin@solcial.io>
2026-01-02 20:38:06 +0100

    tmdb queries

--- src/utils/tmdb.js
+++ src/utils/tmdb.js
@@ -40,29 +40,36 @@ export async function fetchEpisodes(id, season) {
 		.catch(console.error)
 }
 
-export async function fetchTV({ query } = {}) {
+export async function fetchTV({ query, year, id } = {}) {
 	const options = {
 		method: 'GET',
 		headers: await getHeaders(),
 	}
 
-	return fetch('https://api.themoviedb.org/3/search/tv?' + new URLSearchParams({
-		query,
+	const params = {
 		include_adult: 'false',
 		language: 'fr-FR',
-	}).toString(), options)
-		.then(res => res.json())
-		.then(res => {
-			const result = res.results[0]
-			if (result) {
-				return {
-					id: result.id,
-					title: result.name,
-					year: new Date(result.first_air_date).getFullYear().toString(),
-					background: 'https://image.tmdb.org/t/p/w780' + result.backdrop_path,
-				}
+	}
+	if (query) params.query = query
+	if (year) params.year = year
+	const req = !id ? 
+	fetch(
+		'https://api.themoviedb.org/3/search/tv?' + new URLSearchParams(params), options
+	).then(res => res.json()).then(res => res.results[0]) 
+		: 
+	fetch('https://api.themoviedb.org/3/tv/?' + new URLSearchParams({
+		language: 'fr-FR',
+	}), options).then(res => res.json())
+
+	return req.then(result => {
+		if (result && result.id) {
+			return {
+				id: result.id,
+				title: result.name,
+				year: new Date(result.first_air_date).getFullYear().toString(),
+				background: 'https://image.tmdb.org/t/p/w780' + result.backdrop_path,
 			}
-			return null
-		})
-		.catch(err => console.error(err))
+		}
+		return null
+	}).catch(err => console.error(err))
 }
